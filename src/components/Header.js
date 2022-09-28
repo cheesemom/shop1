3f52e184acc110e761a045556ceab2ad2b709d53
cheesemom <smsm2610@naver.com>
2022-09-28 15:15:11 +0900

    0928

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import MainNav from './MainNav'
 import TopBanner from './TopBanner'
 import '../css/Header.scss'
 import { FiShoppingCart, FiUserPlus, FiSearch } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ cart }) => {
     const [on, setOn] = useState(false);
     useEffect(() => {
         const scrollEvent = () => {
@@ -18,13 +19,12 @@ const Header = () => {
     }, [])
     return (
         <header className={`Header ${on ? 'on' : ''}`}>
-            {console.log(on)}
             <TopBanner />
             <div className='gnb'>
                 <h1>
-                    <a href="/">
+                    <Link to="/">
                         <img src={process.env.PUBLIC_URL + '/assets/img/top_logo.png'} alt="" />
-                    </a>
+                    </Link>
                 </h1>
                 <nav className='inner'>
                     <MainNav />
@@ -33,7 +33,7 @@ const Header = () => {
                     <li><FiUserPlus /></li>
                     <li>
                         <FiShoppingCart />
-                        <span>0</span>
+                        <span>{cart.length}</span>
                     </li>
                     <li><FiSearch /></li>
                 </ul>
